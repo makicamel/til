49eaa989b313e83c8849c72c38f74f1f0bb04b09
makicamel <unright@gmail.com>
2020-05-08 07:51:38 +0900

    Revert "Try to change each stroke to variable"
    This reverts commit 465004153136df0d372fde8c67a7f27b74d04b6d.

--- at_coder/abc051_back_and_forth.rb
+++ at_coder/abc051_back_and_forth.rb
@@ -2,12 +2,10 @@
 # https://atcoder.jp/contests/abc051/tasks/abc051_c
 
 sx, sy, tx, ty = gets.split.map(&:to_i)
-up = "U" * (ty - sy)
-right = "R" * (tx - sx)
-down = "D" * (ty - sy)
-left = "L" * (tx - sx)
-a = up + right
-a += down + left
-a += "LU#{up}#{right}RD"
-a += "RD#{down}#{left}LU"
+x = (tx - sx)
+y = (ty - sy)
+a = "U" * y + "R" * x
+a += "D" * y + "L" * x
+a += "L" + "U" * (y + 1) + "R" * (x + 1) + "D"
+a += "R" + "D" * (y + 1) + "L" * (x + 1) + "U"
 puts a
