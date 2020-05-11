dba6f9fba4cb3e40da39de5b98b483b656302363
makicamel <unright@gmail.com>
2020-05-12 07:22:18 +0900

    Change algorithm

--- at_coder/abc061_c_big_array.rb
+++ at_coder/abc061_c_big_array.rb
@@ -2,10 +2,17 @@
 # https://atcoder.jp/contests/abc061/tasks/abc061_c
 
 n, k = gets.split.map(&:to_i)
-array = [].tap do |arr|
-  n.times do
-    a, b = gets.split.map(&:to_i)
-    a.times { arr << b }
+
+hash = {}
+n.times do
+  a, b = gets.split.map(&:to_i)
+  hash[a] = hash.has_key?(a) ? hash[a] + b : b
+end
+
+hash.sort.each do |a, b|
+  k -= b
+  if k <= 0
+    puts a
+    break
   end
-end.sort
-puts array[k + 1]
+end
