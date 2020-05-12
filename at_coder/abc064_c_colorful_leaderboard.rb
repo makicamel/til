b74960926296c1378da63e498aa1f95bfd6219b0
makicamel <unright@gmail.com>
2020-05-13 08:33:50 +0900

    Rate upgrades with point 400

--- at_coder/abc064_c_colorful_leaderboard.rb
+++ at_coder/abc064_c_colorful_leaderboard.rb
@@ -3,20 +3,15 @@
 
 gets
 as = gets.split.map(&:to_i)
-rates = Array.new(8, nil)
+rates = Array.new 8
 free = 0
 
 as.each do |a|
-  case a
-  when 1..399 then    rates[0] = 1
-  when 400..799 then  rates[1] = 1
-  when 800..1199 then rates[2] = 1
-  when 1200..1599 then rates[3] = 1
-  when 1600..1999 then rates[4] = 1
-  when 2000..2399 then rates[5] = 1
-  when 2400..2799 then rates[6] = 1
-  when 2800..3199 then rates[7] = 1
-  else; free += 1
+  i = a / 400
+  if i < 8
+    rates[i] = 1
+  else
+    free += 1
   end
 end
 
