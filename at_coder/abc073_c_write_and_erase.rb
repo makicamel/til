480d175d7219ab9bbba6df8f90222207790f74ff
makicamel <unright@gmail.com>
2020-05-15 07:39:41 +0900

    Use hash for not searching all array

--- at_coder/abc073_c_write_and_erase.rb
+++ at_coder/abc073_c_write_and_erase.rb
@@ -2,10 +2,11 @@
 # https://atcoder.jp/contests/abc073/tasks/abc073_c
 
 n = gets.to_i
-as = []
-n.times do
-  a = gets.to_i
-  as.include?(a) ? as.delete(a) : as.push(a)
+as = [].tap { |array| n.times { array << gets.to_i } }
+hash = {}
+
+as.each do |a|
+  hash[a] = hash[a] ? nil : 1
 end
 
-puts as.size
+puts hash.select { |_, v| v }.size
