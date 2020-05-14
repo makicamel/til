78916fe90982f0c8a8bcb58b6911463e5dd84326
makicamel <unright@gmail.com>
2020-05-15 07:47:23 +0900

    Cool way to initialize array

--- at_coder/abc073_c_write_and_erase.rb
+++ at_coder/abc073_c_write_and_erase.rb
@@ -2,7 +2,7 @@
 # https://atcoder.jp/contests/abc073/tasks/abc073_c
 
 n = gets.to_i
-as = [].tap { |array| n.times { array << gets.to_i } }
+as = Array.new(n) { gets.to_i }
 hash = {}
 
 as.each do |a|
