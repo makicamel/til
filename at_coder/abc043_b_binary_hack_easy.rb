4b099907ceeb8c57e81033f0c381f9976027f3ef
makicamel <unright@gmail.com>
2020-05-19 08:28:15 +0900

    Write in short way

--- at_coder/abc043_b_binary_hack_easy.rb
+++ at_coder/abc043_b_binary_hack_easy.rb
@@ -4,13 +4,6 @@
 s = gets.chomp
 
 answer = []
-s.chars.each do |char|
-  case char
-  when "0", "1"
-    answer << char
-  when "B"
-    answer.pop unless answer.empty?
-  end
-end
+s.chars.each { |char| char == "B" ? answer.pop : answer.push(char) }
 
 puts answer.join
